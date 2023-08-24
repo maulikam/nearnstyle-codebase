39df3ba39f61616b3c3347895f04d6e9c978d3e9
maulikdave <Mom@1432>
2023-08-24 15:38:12 +0530

    refactor oauth

--- nearnstyle-apis/src/main/java/com/nearnstyle/apis/common/security/ResourceServerConfig.java
+++ nearnstyle-apis/src/main/java/com/nearnstyle/apis/common/security/ResourceServerConfig.java
@@ -31,19 +31,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .antMatchers("/api/otp/**").permitAll()
                 .antMatchers("/api/users/register").permitAll()
                 .antMatchers("/api/salons/salon/**").permitAll()
-                .antMatchers("/imtecho-ui/**").permitAll()
                 .antMatchers("/api/mobile/**").permitAll()
-                .antMatchers("/api/cowin/**").permitAll()
-                .antMatchers("/api/member/**").permitAll()
-                .antMatchers("/api/sheetload/**").permitAll()
-                .antMatchers("/api/insert_user_analytics_details/**").permitAll()
                 .antMatchers("/api/user/forgotpassword/**").permitAll()
-                .antMatchers("/api/systemconfigsync/**").permitAll()
-                .antMatchers("/api/digilocker/**").permitAll()
-                .antMatchers("/api/ndhm/healthid/create/getHealthIdUserDetailsByDownloadCode/**").permitAll()
-                .antMatchers("/api/ndhm/healthid/create/getfile/**").permitAll()
-                .antMatchers("/api/deworming/**").permitAll()
-                .antMatchers("/api/cmdashboard/**").access("#oauth2.hasScope('cm-dashboard')")
                 .antMatchers("/api/**").access("#oauth2.hasScope('write')");
         http.headers().frameOptions().sameOrigin();
         http.csrf().disable();
