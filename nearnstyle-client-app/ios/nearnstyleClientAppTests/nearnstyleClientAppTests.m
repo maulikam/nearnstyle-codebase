47f8f22683f62ca99b38b489d3dfb0f0466e4f26
rutvik-shukla <142991891+rutvik-shukla@users.noreply.github.com>
2023-08-31 00:18:17 +0530

    Store List Screen with React Native Element UI Lib (#36)

--- nearnstyle-client-app/ios/nearnstyleClientAppTests/nearnstyleClientAppTests.m
+++ nearnstyle-client-app/ios/nearnstyleClientAppTests/nearnstyleClientAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface nearnstyleClientAppTests : XCTestCase
+@interface nearnstyleclientappTests : XCTestCase
 
 @end
 
-@implementation nearnstyleClientAppTests
+@implementation nearnstyleclientappTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
